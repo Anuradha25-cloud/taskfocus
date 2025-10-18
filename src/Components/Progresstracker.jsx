97cac9f7120a7fa9eece8fa52fb27cf013724f71
Anuradha25-cloud <anureddy590@gmail.com>
2025-10-18 10:43:38 +0530

    Initial commit

--- src/Components/Progresstracker.jsx
+++ src/Components/Progresstracker.jsx
@@ -1,7 +1,10 @@
-export default function TaskList() {
+  export default function Progresstracker() {
+  const completedTasks = tasks.filter((t) => t.completed).length;
+      const totaltasks = tasks.length;
+      const percentage = totaltasks == 0 ? 0: (completedTasks/totaltasks) * 100;
     return (
-        <div>
-            <h1>Progress Tracker Component</h1>
-        </div>
-    )
+      <div>
+        
+      </div>
+  );
 }
